4df98e435e9ae092151f03b620c51dd284c1da62
John Titor <xionovermazes@yahoo.com>
2016-06-28 14:48:36 +0300

    Added vector template class

---
+++ include/nerdy/structs/Vector2.h
@@ -0,0 +1,17 @@
+namespace nd
+{
+
+template <typename t>
+struct Vector2
+{
+
+t x;
+t y;
+
+};
+
+typedef Vector2<float> Vector2f;
+typedef Vector2<int> Vector2i;
+typedef Vector2<double> Vector2d;
+
+}
